2bded0984ca651ca48c39776b1252740a8603f0b
Apples <dbralir@gmail.com>
2021-01-31 16:29:30 -0600

    Reverted questionable change

--- src/audiosource/wav/stb_vorbis.c
+++ src/audiosource/wav/stb_vorbis.c
@@ -609,7 +609,6 @@ enum STBVorbisError
       #define __forceinline
    #endif
 #endif
-#define __forceinline
 
 #if STB_VORBIS_MAX_CHANNELS > 256
 #error "Value of STB_VORBIS_MAX_CHANNELS outside of allowed range"
